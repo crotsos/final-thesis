709b6f5065a83b797f8644f958385bda38b595b2
crotsos <cr409@cl.cam.ac.uk>
2014-04-12 13:20:39 +0100

    Last pass on Intro

---
+++ Chapter1/Chapter1Figs/sdnsim-use-case.gpi.d
@@ -0,0 +1,5 @@
+# vim: ft=make
+ifndef INCLUDED_Chapter1__Chapter1Figs__sdnsim-use-case_gpi_d
+INCLUDED_Chapter1__Chapter1Figs__sdnsim-use-case_gpi_d := 1
+Chapter1/Chapter1Figs/sdnsim-use-case.pdf Chapter1/Chapter1Figs/sdnsim-use-case.gpi.d:  Chapter1/Chapter1Figs/one-layer.data Chapter1/Chapter1Figs/one-layer.data Chapter1/Chapter1Figs/two-layer.data Chapter1/Chapter1Figs/two-layer.data 
+endif
---
+++ Introduction/introduction.d
@@ -0,0 +1,4 @@
+# vim: ft=make
+.PHONY: Introduction/introduction._graphics
+Introduction/introduction.aux Introduction/introduction.aux.make Introduction/introduction.d Introduction/introduction.pdf: $(call path-norm,Introduction/introduction.tex)
+.SECONDEXPANSION:
